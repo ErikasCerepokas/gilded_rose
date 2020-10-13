20bec4bd935b454a6c710e9d1cefb7b9e6eed6c3
ErikasCerepokas <erikascerepokas@yahoo.com>
2020-10-13 19:19:08 +0300

    shortened the code with simple +-= signs

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,3 +1,5 @@
+
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -8,36 +10,27 @@ class GildedRose(object):
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        item.quality -= 1
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality += 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality += 1
                         if item.sell_in < 6:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality += 1
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                item.quality -= 1
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality -= item.quality
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+                        item.quality += 1
