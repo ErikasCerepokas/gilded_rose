315d3b9e8264981d0fc8b26c5b296703ba262406
ErikasCerepokas <erikascerepokas@yahoo.com>
2020-10-13 22:14:43 +0300

    27line moved to 36

--- gilded_rose.py
+++ gilded_rose.py
@@ -24,8 +24,7 @@ class GildedRose(object):
             else:
                 if item.quality > 0:
                     item.quality -= 1
-            item.sell_in -= 1
-            if item.sell_in < 0:
+            if item.sell_in < 1:
                 if item.name == "Aged Brie":
                     if item.quality < 50:
                         item.quality += 1
@@ -34,4 +33,5 @@ class GildedRose(object):
                 else:
                     if item.quality > 0:
                         item.quality -= 1
+            item.sell_in -= 1
 
