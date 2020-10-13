18fddad1062ba998fd7c3a41380c71929ff1b2fb
ErikasCerepokas <erikascerepokas@yahoo.com>
2020-10-13 19:37:12 +0300

    Quality decrease function

--- gilded_rose.py
+++ gilded_rose.py
@@ -9,9 +9,8 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality -= 1
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    self.quality_decrease()
             else:
                 if item.quality < 50:
                     item.quality += 1
@@ -25,9 +24,8 @@ class GildedRose(object):
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality -= 1
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            self.quality_decrease()
                     else:
                         item.quality -= item.quality
                 else:
@@ -36,3 +34,7 @@ class GildedRose(object):
     def quality_increase(self):
         if item.quality < 50:
             item.quality += 1
+
+    def quality_decrease(self):
+        if item.quality > 0:
+            item.quality -= 1
