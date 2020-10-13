d1381714706a32c737c1d92fbccf076a47f09f19
ErikasCerepokas <erikascerepokas@yahoo.com>
2020-10-13 22:07:09 +0300

    Started again from beggining, compressed bot part

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,46 +1,37 @@
-import item
-
 
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
-    def item_loop(self):
+    def update_quality(self):
         for item in self.items:
-            self.update_quality(item)
-
-    def update_quality(self,item):
-        self.sellin()
-        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                self.quality_decrease()
-        else:
-            if item.quality < 50:
-                item.quality += 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        self.quality_increase()
-                    if item.sell_in < 6:
-                        self.quality_increase()
-        if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        self.quality_decrease()
-                else:
-                    item.quality -= item.quality
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+            if item.name == "Aged Brie":
+                    if item.quality < 50:
+                        item.quality += 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality < 50:
+                        item.quality += 1
+                        if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                            if item.sell_in < 11:
+                                if item.quality < 50:
+                                    item.quality += 1
+                            if item.sell_in < 6:
+                                if item.quality < 50:
+                                    item.quality += 1
             else:
-                self.quality_increase()
-
-    def quality_increase(self):
-        if item.quality < 50:
-            item.quality += 1
-
-    def quality_decrease(self):
-        if item.quality > 0:
-            item.quality -= 1
-
-    def sellin(self):
-        if item.name != "Sulfuras, Hand of Ragnaros":
+                if item.quality > 0:
+                    item.quality -= 1
             item.sell_in -= 1
+            if item.sell_in < 0:
+                if item.name == "Aged Brie":
+                    if item.quality < 50:
+                        item.quality += 1
+                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = item.quality - item.quality
+                else:
+                    if item.quality > 0:
+                        item.quality -= 1
+
