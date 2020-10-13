2e9b1ac6ceebe92f0e63d5c126d8d7fde6ccc5e7
ErikasCerepokas <erikascerepokas@yahoo.com>
2020-10-13 22:27:30 +0300

    line from two if submerged with and tag

--- gilded_rose.py
+++ gilded_rose.py
@@ -9,28 +9,23 @@ class GildedRose(object):
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
             if item.name == "Aged Brie":
-                    if item.quality < 50:
-                        item.quality += 1
-                    if item.sell_in < 1:
-                        if item.quality < 50:
-                            item.quality += 1
+                if item.quality < 50:
+                    item.quality += 1
+                if item.sell_in < 1 and item.quality < 50:
+                    item.quality += 1
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality < 50:
-                        item.quality += 1
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality += 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality += 1
-                    if item.sell_in < 1:
-                        item.quality = item.quality - item.quality
+                if item.quality < 50:
+                     item.quality += 1
+                if item.sell_in < 11 and item.quality < 50:
+                    item.quality += 1
+                if item.sell_in < 6 and item.quality < 50:
+                    item.quality += 1
+                if item.sell_in < 1:
+                    item.quality = item.quality - item.quality
             else:
                 if item.quality > 0:
                     item.quality -= 1
-                if item.sell_in < 1:
-                    if item.quality > 0:
-                        item.quality -= 1
-
+                if item.sell_in < 1 and item.quality > 0:
+                    item.quality -= 1
             item.sell_in -= 1
 
